1e8241d9e321e6267f3d5514b172d9f1aacb3e16
Şivan Miroğlu <sivanmiroglu854@gmail.com>
2026-01-19 21:58:00 +0300

    Add customer list and input validation logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Csharp.oop.Bankapp;
+﻿using System.Data;
+
+namespace Csharp.oop.Bankapp;
 class Program
 {
     static void Main(string[] args)
@@ -8,9 +10,44 @@ class Program
 
         // Show Main Menu
         ShowMainMenu();
+        
     }
+     static List<Customer> customers = new List<Customer>(); // We will keep all customers here
+     static int nextCustomerId= 1; // We will give a number to each new customer
+     static void RegisterNewCustomer()
+    {
+        Console.WriteLine("\n════════ YENİ MÜŞTERİ KAYDI ════════");
 
-    static void ShowMainMenu()
+        // We receive information from the user
+        Console.Write("Ad Soyad: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Telefon:");
+        string phone = Console.ReadLine();
+
+        Console.Write("Başlangıç Bakiyesi:");
+        decimal balance ;
+
+        // We check if the correct number has been entered with TryParse
+        while(!decimal.TryParse(Console.ReadLine(), out balance) || balance < 0)
+        {
+            Console.Write("Geçersiz bakiye! Lütfen pozitif bir sayı girin:");
+        }
+
+        // We create a new customer object
+        Customer newCustomer = new Customer(nextCustomerId,name,phone,balance);
+
+        // We add the new customer to the list
+        customers.Add(newCustomer);
+
+        nextCustomerId++; // We increase the customer number for the next customer
+    Console.WriteLine($"\n✅ Müşteri başarıyla kaydedildi!");
+        Console.WriteLine("Customer ID: {newCustomer.Id}");
+
+        // Wait 2 seconds and return to the menu
+        System.Threading.Thread.Sleep(2000);
+    }
+   static void ShowMainMenu()
     {
         Console.WriteLine("\n════════════ ANA MENÜ ════════════");
         Console.WriteLine("1. Yeni Müşteri Kaydı");
